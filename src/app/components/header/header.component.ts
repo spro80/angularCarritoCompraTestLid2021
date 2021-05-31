f3f270124c21130b574f3fd244ce5fc549209946
David D <dsdiazs@falabella.cl>
2021-05-31 04:04:10 -0400

    add method clean shopping cart and validate elements shopping cart

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -26,18 +26,48 @@ export class HeaderComponent implements OnInit {
   }
 
   public openModal(modalId: string) {
-    this.modalOpen = true;
-    this.modalInformativeOpen = true;
-    const title = '';
-    const information = '';
-    const nameButton = '';
-    this.infoModal = new StructureModalWindow(title, information, nameButton, modalId, false);
+
+    if( this.validateShoppinCart() ) {
+      this.modalOpen = true;
+      this.modalInformativeOpen = true;
+      const title = '';
+      const information = '';
+      const nameButton = '';
+      this.infoModal = new StructureModalWindow(title, information, nameButton, modalId, false);
+    }
+
   }
+
+  validateShoppinCart() {
+    let a = JSON.parse(sessionStorage.getItem('infoCart') || '{}' );
+    let validate = false;
+    if( a.length === undefined ) {
+      alert("todo: PENDING CREATE OTHER MODAL WITH MESSAGE.  There is not elements in the shopping cart. ");
+      validate = false;
+    }else {
+      validate = true;
+    }
+
+    return validate;
+  }
+
 
   action(action: 'pos' | 'neg') {
     if (action === 'neg') {
       this.modalOpen = false;
     }
+  }
+
+  cleanShoppingCart() {
+    //localStorage.removeItem('infoCart');
+    sessionStorage.removeItem('infoCart');
+    console.log( JSON.parse(sessionStorage.getItem('infoCart') || '{}' ) );
+
+    sessionStorage.removeItem('totalCartModal');
+
+    sessionStorage.removeItem('totalDiscountCartModal');
+
+    sessionStorage.removeItem('totalWithDiscountCartModal');
   }
 
 }
