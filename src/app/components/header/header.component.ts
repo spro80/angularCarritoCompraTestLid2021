ed0bac14241a938a649e3814a253bea4788c7399
David D <dsdiazs@falabella.cl>
2021-05-31 01:15:23 -0400

    add called to modal in button cart that is in component headers

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StructureModalWindow } from 'src/app/components/modal-window/StructureModalWindow';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+  message: String = '';
+  showModalBox: boolean = false;
+
+  infoModal: StructureModalWindow = new StructureModalWindow('', '', '', '', false);
+  modalInformativeOpen: boolean = false;
+  modalOpen: boolean = false;
+
+  titleModal: string = '';
+  informationModal: string = '';
+  namePosCondition: string = '';
+  nameNegCondition: string= '';
 
   ngOnInit(): void {
+
+  }
+
+  public openModal(modalId: string) {
+    this.modalOpen = true;
+    this.modalInformativeOpen = true;
+    const title = '';
+    const information = '';
+    const nameButton = '';
+    this.infoModal = new StructureModalWindow(title, information, nameButton, modalId, false);
+  }
+
+  action(action: 'pos' | 'neg') {
+    if (action === 'pos'){
+      console.log('presiono ok');
+    } else if (action === 'neg') {
+      this.modalOpen = false;
+    }
   }
 
 }
