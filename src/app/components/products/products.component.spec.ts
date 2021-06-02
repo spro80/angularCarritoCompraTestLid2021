00ee97f76693a2fc20d159e564fe7cb959545e1f
David D <dsdiazs@falabella.cl>
2021-06-02 03:48:59 -0400

    uni test for products

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,27 +1,79 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-//import { ProductsService } from './../../../app/modules/product/services/products.service'
-
+import { Component, Input, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+//import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { ProductsComponent } from './products.component';
+import { ProductsService } from './../../../app/modules/product/services/products.service'
+
+
+import { Observable, of } from 'rxjs';
+
+
+
+class MockProductService {
+  public response = {
+    status: {
+      code: 2000,
+      message: 'success'
+    }
+  };
+
+  /*productService(): Observable<any> {
+    return of(this.response);
+  }*/
+
+  
+
+  getProducts$(): Observable<any> {
+    return of(this.response);
+  }
+
+  getDiscounts(): Observable<any> {
+    return of(this.response);
+  }
+
+  
+
+}
+
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
-  let fixture: ComponentFixture<ProductsComponent>;
+  //let fixture: ComponentFixture<ProductsComponent>;
+  let productsService: ProductsService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProductsComponent ]
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProductsComponent 
+      ],
+      imports: [
+        HttpClientModule
+      ],
+      providers: [
+        ProductsComponent,
+        { provide: ProductsService, useClass: MockProductService }
+      ],
     })
     .compileComponents();
+    component = TestBed.get(ProductsComponent);
+    productsService = TestBed.get(ProductsService);
   });
 
+  /*
   beforeEach(() => {
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });*/
+
+  it('should create', inject([ProductsComponent], () => {
+    expect(component).toBeTruthy();
+  }));
+
+
+  it.skip('ngOnInit', () => {
+    component.ngOnInit();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
   
 });
